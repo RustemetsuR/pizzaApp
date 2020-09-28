3c25914da345bc0db4016e45d1fb2d6493bd4e27
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-09-28 14:45:15 +0600

    Added rootReducer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import pizzasReducer from './store/reducers/pizzasReducer';
+import ordersReducer from './store/reducers/ordersReducer';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootReducer = combineReducers({
+  pizzas: pizzasReducer,
+  orders: ordersReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
+
+ReactDOM.render( app,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
